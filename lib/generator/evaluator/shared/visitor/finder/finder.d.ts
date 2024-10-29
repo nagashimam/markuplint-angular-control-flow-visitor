62e040998b6157dabd33b194d169237bf2190255
Masato Nagashima <masato.ngsm@gmail.com>
2024-10-29 20:31:58 +0900

    return文が不要だったので削除

--- lib/generator/evaluator/shared/visitor/finder/finder.d.ts
+++ lib/generator/evaluator/shared/visitor/finder/finder.d.ts
@@ -1,18 +1,5 @@
-import { TmplAstElement, TmplAstIfBlock, TmplAstRecursiveVisitor, TmplAstTemplate, TmplAstDeferredBlock, TmplAstDeferredBlockPlaceholder, TmplAstDeferredBlockError, TmplAstDeferredBlockLoading, TmplAstSwitchBlock, TmplAstSwitchBlockCase, TmplAstForLoopBlock, TmplAstForLoopBlockEmpty, TmplAstIfBlockBranch, TmplAstContent } from "@angular/compiler";
+import { TmplAstRecursiveVisitor } from "@angular/compiler";
 export declare abstract class Finder extends TmplAstRecursiveVisitor {
     protected _hasFound: boolean;
     get hasFound(): boolean;
-    visitElement(element: TmplAstElement): void;
-    visitTemplate(template: TmplAstTemplate): void;
-    visitDeferredBlock(deferred: TmplAstDeferredBlock): void;
-    visitDeferredBlockPlaceholder(block: TmplAstDeferredBlockPlaceholder): void;
-    visitDeferredBlockError(block: TmplAstDeferredBlockError): void;
-    visitDeferredBlockLoading(block: TmplAstDeferredBlockLoading): void;
-    visitSwitchBlock(block: TmplAstSwitchBlock): void;
-    visitSwitchBlockCase(block: TmplAstSwitchBlockCase): void;
-    visitForLoopBlock(block: TmplAstForLoopBlock): void;
-    visitForLoopBlockEmpty(block: TmplAstForLoopBlockEmpty): void;
-    visitIfBlock(block: TmplAstIfBlock): void;
-    visitIfBlockBranch(block: TmplAstIfBlockBranch): void;
-    visitContent(content: TmplAstContent): void;
 }
--- src/generator/evaluator/shared/visitor/finder/finder.ts
+++ src/generator/evaluator/shared/visitor/finder/finder.ts
@@ -1,77 +1,10 @@
 import {
-  TmplAstElement,
-  TmplAstIfBlock,
   TmplAstRecursiveVisitor,
-  TmplAstTemplate,
-  TmplAstDeferredBlock,
-  TmplAstDeferredBlockPlaceholder,
-  TmplAstDeferredBlockError,
-  TmplAstDeferredBlockLoading,
-  TmplAstSwitchBlock,
-  TmplAstSwitchBlockCase,
-  TmplAstForLoopBlock,
-  TmplAstForLoopBlockEmpty,
-  TmplAstIfBlockBranch,
-  TmplAstContent,
 } from "@angular/compiler";
 
 export abstract class Finder extends TmplAstRecursiveVisitor {
   protected _hasFound = false;
   public get hasFound() {
     return this._hasFound;
-  }
-  override visitElement(element: TmplAstElement): void {
-    if (this.hasFound) return;
-    super.visitElement(element);
-  }
-  override visitTemplate(template: TmplAstTemplate): void {
-    if (this.hasFound) return;
-    super.visitTemplate(template);
-  }
-  override visitDeferredBlock(deferred: TmplAstDeferredBlock): void {
-    if (this.hasFound) return;
-    super.visitDeferredBlock(deferred);
-  }
-  override visitDeferredBlockPlaceholder(
-    block: TmplAstDeferredBlockPlaceholder,
-  ): void {
-    if (this.hasFound) return;
-    super.visitDeferredBlockPlaceholder(block);
-  }
-  override visitDeferredBlockError(block: TmplAstDeferredBlockError): void {
-    if (this.hasFound) return;
-    super.visitDeferredBlockError(block);
-  }
-  override visitDeferredBlockLoading(block: TmplAstDeferredBlockLoading): void {
-    if (this.hasFound) return;
-    super.visitDeferredBlockLoading(block);
-  }
-  override visitSwitchBlock(block: TmplAstSwitchBlock): void {
-    if (this.hasFound) return;
-    super.visitSwitchBlock(block);
-  }
-  override visitSwitchBlockCase(block: TmplAstSwitchBlockCase): void {
-    if (this.hasFound) return;
-    super.visitSwitchBlockCase(block);
-  }
-  override visitForLoopBlock(block: TmplAstForLoopBlock): void {
-    if (this.hasFound) return;
-    super.visitForLoopBlock(block);
-  }
-  override visitForLoopBlockEmpty(block: TmplAstForLoopBlockEmpty): void {
-    if (this.hasFound) return;
-    super.visitForLoopBlockEmpty(block);
-  }
-  override visitIfBlock(block: TmplAstIfBlock): void {
-    if (this.hasFound) return;
-    super.visitIfBlock(block);
-  }
-  override visitIfBlockBranch(block: TmplAstIfBlockBranch): void {
-    if (this.hasFound) return;
-    super.visitIfBlockBranch(block);
-  }
-  override visitContent(content: TmplAstContent): void {
-    if (this.hasFound) return;
-    super.visitContent(content);
   }
 }
